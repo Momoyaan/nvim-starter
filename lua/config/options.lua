73ecb3b0f93380d1d5f2ce1cebd6f8e0c5b4d6d4
Momoyaan <ianmathewaviso@gmail.com>
2025-09-21 19:00:36 +0800

    Sep 21

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,22 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.snacks_animate = false
+vim.g.background = "dark"
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyReload",
+  callback = function()
+    vim.schedule(function()
+      local core = require("plugins.core")
+      local colorscheme = nil
+      for _, plugin in ipairs(core) do
+        if plugin[1] == "LazyVim/LazyVim" and plugin.opts then
+          colorscheme = plugin.opts.colorscheme
+          break
+        end
+      end
+      if colorscheme then
+        vim.cmd.colorscheme(colorscheme)
+      end
+    end)
+  end,
+})
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,38 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  -- { "ellisonleao/gruvbox.nvim" },
+  -- { "nyoom-engineering/oxocarbon.nvim" },
+  -- { "cranberry-clockworks/coal.nvim" },
+  {
+    "nvim-mini/mini.base16",
+    config = function()
+      require("mini.base16").setup({
+        palette = {
+          base00 = "#0A0A0A",
+          base01 = "#0A0A0A",
+          base02 = "#262626",
+          base03 = "#737373",
+          base04 = "#a3a3a3",
+          base05 = "#fafafa",
+          base06 = "#FFFFFF",
+          base07 = "#62c073",
+          base08 = "#f75f8f",
+          base09 = "#ff9907",
+          base0A = "#ee5396",
+          base0B = "#52a8ff",
+          base0C = "#c472fb",
+          base0D = "#1da9b0",
+          base0E = "#be95ff",
+          base0F = "#82cfff",
+        },
+      })
+    end,
+  },
 
   -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "coal",
+  --   },
+  -- },
 }
